4a43d964639559a77cc0f95bc7fca1c445a0ddf0
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-04-29 11:44:12 +0530

    feat: add missing doctype, reports and dashboard

---
+++ fitness_wellness/fitness_wellness/doctype/fitness_wellness_settings/fitness_wellness_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Antigravity and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Fitness Wellness Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ fitness_wellness/reports_analytics/report/trainer_performance_report/trainer_performance_report.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Antigravity and contributors
+# For license information, please see license.txt
+
+import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
